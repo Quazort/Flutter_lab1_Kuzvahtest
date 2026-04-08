f5ce01314d5363a41827969759bdbfc9d0413d6e
alincqeek <pstartceva@yandex.ru>
2026-04-08 11:39:36 +0300

    pre reliz kuzvah

--- bin/todo_app.dart
+++ bin/todo_app.dart
@@ -1,29 +1,58 @@
+import 'dart:io';
+import 'package:todo_app/todo.dart';
+import 'package:todo_app/todo_repository.dart';
+
 void main() {
-  print("helloworlt");
-  String name = "Artur";
-  String? name2 = null;
-  var count = 0;
-  var title = "f";
-  var x = 5;
-  final id = 5;
-  const appName = "tioduy";
-  print("$name");
-  List<String> tags = ['bycnb', 'ljv'];
-  Map<String, dynamic> data = {'key': 'value', 'num': '10'};
-  Set<int> numbers = {1, 2, 3};
-  String greet(String name) => 'Hello $name';
-  printTodo(title: "buy food");
-  printTodo(title: "do homework", done: true);
+  TodoRepository todo = TodoRepository();
+  printMenu();
+  while (true) {
+    stdout.write("> ");
+    String? input = stdin.readLineSync();
+    if (input == null) {
+      continue;
+    }
+    input = input.trim();
+    if (input.isEmpty) {
+      continue;
+    }
+    bool exit = handleCommand(repo, input);
+    if (exit) {
+      break;
+    }
+  }
 }
 
-void printTodo({required String title, bool done = false}) {
-  print("${done ? '+' : '-'} $title");
+bool handleCommand(repo, String input) {}
+
+void printMenu() {
+  print('Приложение ТОДО');
+  print("Команды");
+  print('add <text> - добавить задачу');
+  print('list       - показать список');
+  print('done<id>   - отметить выполненной');
+  print('delete<id> - удалить задачу');
+  print('exit       - выход');
+  print('---------------------------------');
+  print(' ');
 }
 
-class Todo {
-  final int id;
-  String title;
-  bool isDone;
+void addCommand(TodoRepository repo, String input) {
+  if (input.length <= 4) {
+    print('Ошибка: введите текст');
+    return;
+  }
+  String title = input.substring(4).trim();
+  repo.add(title);
+  print("Задача добавлена");
+}
 
-  Todo({required this.id, required this.title, this.isDone = false});
+void listCommand(TodoRepository repo) {
+  List<Todo> todos = repo.getAll();
+  if (todos.isEmpty) {
+    print("Список пуст");
+    return;
+  }
+  for (var todo in todos) {
+    print(todo);
+  }
 }
